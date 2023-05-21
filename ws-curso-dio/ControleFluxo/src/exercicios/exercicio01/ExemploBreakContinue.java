fc58c3241b83cf3abe3dbf03634ee670ecd0379c
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-05-20 23:28:13 -0300

    novos exercicios

---
+++ ws-curso-dio/ControleFluxo/src/exercicios/exercicio01/ExemploBreakContinue.java
@@ -0,0 +1,29 @@
+package exercicios.exercicio01;
+
+public class ExemploBreakContinue {
+
+	public static void main(String[] args) {
+		
+		// Exemplo 1
+		for (int numero = 1; numero <= 5 ; numero++) {
+			if(numero == 3) {
+				continue;
+			}
+			
+			System.out.println(numero);
+		}
+		
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		// Exemplo 2
+		for (int numero = 1; numero <= 5 ; numero++) {
+			if(numero == 3) {
+				break;
+			}
+			
+			System.out.println(numero);
+		}
+	}
+}
---
+++ ws-curso-dio/ControleFluxo/src/exercicios/exercicio01/ExemploFor.java
@@ -0,0 +1,13 @@
+package exercicios.exercicio01;
+
+public class ExemploFor {
+
+	public static void main(String[] args) {
+		
+		for (int contador = 0; contador <= 20; contador++) {
+			System.out.println("Contador: " + contador);
+		}
+		
+		System.out.println("Chegou ao máximo da contagem!");
+	}
+}
