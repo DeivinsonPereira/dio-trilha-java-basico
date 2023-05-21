97655d9decc4b128e10b672968fbc9663271f8cd
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-05-20 22:41:55 -0300

    Commit Inicial

---
+++ ws-curso-dio/ControleFluxo/src/exercicios/exercicio01/CaixaEletonico.java
@@ -0,0 +1,14 @@
+package exercicios.exercicio01;
+
+public class CaixaEletonico {
+
+	public static void main(String[] args) {
+		double saldo = 25.0;
+		double valorSolicitado = 17.0;
+		
+		if(valorSolicitado < saldo)
+			saldo = saldo - valorSolicitado;
+		
+		System.out.printf("Saldo atual: R$%.2f", saldo);
+	}
+}
